5fc189bf0aa9ff3977b4dbccedb92e3123780d53
Jesse Clark <wjesseclark@gmail.com>
2016-12-13 18:46:45 -0500

    added initial docs for website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var today = 'tuesday'
+
+var number = 10
+
+var basic = true 
+
+var fruit = ['apples', 'oranges', 'pears'];
+
+for (var i = 0; i < fruit.length; i++){
+	console.log(fruit[i]);
+}
+
